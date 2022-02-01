5674161107ba1f2a4cea22d393b0ce0d78dc8fde
Rodrick <rialcantara21@gmail.com>
2022-02-02 01:22:13 +0800

    chore: update redux and add actions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,22 +2,25 @@ import Layout from 'components/Layout';
 import React from 'react';
 import { useAsyncFn } from 'react-use';
 import { APP_ZONE } from 'shared/configs/App';
-import { useAppDispatch, useAppSelector } from 'shared/hooks/useRedux';
-import { testIncrementAsync } from 'shared/redux/Test/Thunks';
+import { useAppSelector } from 'shared/hooks/useRedux';
+import { TestActions } from 'shared/redux/Test/Actions';
 
 const IndexPage: React.FC = () => {
-  const dispatch = useAppDispatch();
+  const { increment } = TestActions();
   const count = useAppSelector(state => state.tests.count);
 
-  console.log('APP_ZONE: ', APP_ZONE);
-
   const [, incrementAsync] = useAsyncFn(async () => {
-    await dispatch(testIncrementAsync());
+    increment();
   });
+
+  console.log('APP_ZONE: ', APP_ZONE);
+  console.log('count: ', count);
 
   return (
     <Layout>
-      Index Page <button onClick={incrementAsync}>Increment</button> | count: {count}
+      Index Page
+      <button onClick={incrementAsync}>Increment</button>
+      <div>count: {count}</div>
     </Layout>
   );
 };
--- src/shared/redux/store.ts
+++ src/shared/redux/store.ts
@@ -4,14 +4,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import { APP_ZONE } from 'shared/configs/App';
 
-const createStore = () => {
-  return configureStore({
-    reducer: {
-      tests: TestSlice,
-    },
-    middleware: getDefaultMiddleware =>
-      APP_ZONE !== 'production' ? getDefaultMiddleware().concat(logger) : getDefaultMiddleware(),
-  });
-};
+const config = configureStore({
+  reducer: {
+    tests: TestSlice,
+  },
+  middleware: getDefaultMiddleware =>
+    APP_ZONE !== 'production' ? getDefaultMiddleware().concat(logger) : getDefaultMiddleware(),
+});
+
+const createStore = (): typeof config => config;
 
 export default createStore;
