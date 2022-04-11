513287d25b3aa1503ab74e324cc38a0f73d00980
constROD <rialcantara21@gmail.com>
2022-04-11 14:47:20 +0800

    chore: add svgr config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`, 
-    `gatsby-plugin-root-import`
+    `gatsby-plugin-root-import`,
+    {
+			resolve: "gatsby-plugin-svgr",
+			options: {
+				icon: true
+			}
+		}
   ],
 };
