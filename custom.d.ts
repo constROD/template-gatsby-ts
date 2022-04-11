ee6ae8a8125f3f208b576e8cd799eb66643cf49a
constROD <rialcantara21@gmail.com>
2022-04-11 14:22:58 +0800

    chore: add svg config

--- custom.d.ts
+++
@@ -1,5 +0,0 @@
-// This accepts svg assets for typescript
-declare module '*.svg' {
-  const content: string;
-  export default content;
-}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-root-import`],
+  plugins: [
+    `gatsby-plugin-styled-components`, 
+    `gatsby-plugin-root-import`
+  ],
 };
