139023a0dab611ba324f8f2777e3eabeba1dbab1
constROD <rialcantara21@gmail.com>
2022-10-12 01:48:23 +0800

    chore: add image component plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,9 @@ module.exports = {
     `gatsby-plugin-styled-components`, 
     `gatsby-plugin-root-import`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
 			resolve: "gatsby-plugin-svgr",
 			options: {
